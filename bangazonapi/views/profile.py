14855d2fc4e21c97dae58ce28c36e1d55925007e
Joshua Medlen <joshua246@gmail.com>
2021-02-16 21:15:34 -0600

    changed approach and finished creating a POST method in favoritesellers that will let a user favorite a seller

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -228,70 +228,84 @@ class Profile(ViewSet):
 
     @action(methods=['get','post'], detail=False)
     def favoritesellers(self, request):
-        if request.method == "GET":
-            """
-            @api {GET} /profile/favoritesellers GET favorite sellers
-            @apiName GetFavoriteSellers
-            @apiGroup UserProfile
+        """
+        @api {GET} /profile/favoritesellers GET favorite sellers
+        @apiName GetFavoriteSellers
+        @apiGroup UserProfile
 
-            @apiHeader {String} Authorization Auth token
-            @apiHeaderExample {String} Authorization
-                Token 9ba45f09651c5b0c404f37a2d2572c026c146611
+        @apiHeader {String} Authorization Auth token
+        @apiHeaderExample {String} Authorization
+            Token 9ba45f09651c5b0c404f37a2d2572c026c146611
 
-            @apiSuccess (200) {id} id Favorite id
-            @apiSuccess (200) {Object} seller Favorited seller
-            @apiSuccess (200) {String} seller.url Seller URI
-            @apiSuccess (200) {String} seller.phone_number Seller phone number
-            @apiSuccess (200) {String} seller.address Seller address
-            @apiSuccess (200) {String} seller.user Seller user profile URI
-            @apiSuccessExample {json} Success
-                [
-                    {
-                        "id": 1,
-                        "seller": {
-                            "url": "http://localhost:8000/customers/5",
-                            "phone_number": "555-1212",
-                            "address": "100 Endless Way",
-                            "user": "http://localhost:8000/users/6"
-                        }
-                    },
-                    {
-                        "id": 2,
-                        "seller": {
-                            "url": "http://localhost:8000/customers/6",
-                            "phone_number": "555-1212",
-                            "address": "100 Dauntless Way",
-                            "user": "http://localhost:8000/users/7"
-                        }
-                    },
-                    {
-                        "id": 3,
-                        "seller": {
-                            "url": "http://localhost:8000/customers/7",
-                            "phone_number": "555-1212",
-                            "address": "100 Indefatiguable Way",
-                            "user": "http://localhost:8000/users/8"
-                        }
+        @apiSuccess (200) {id} id Favorite id
+        @apiSuccess (200) {Object} seller Favorited seller
+        @apiSuccess (200) {String} seller.url Seller URI
+        @apiSuccess (200) {String} seller.phone_number Seller phone number
+        @apiSuccess (200) {String} seller.address Seller address
+        @apiSuccess (200) {String} seller.user Seller user profile URI
+        @apiSuccessExample {json} Success
+            [
+                {
+                    "id": 1,
+                    "seller": {
+                        "url": "http://localhost:8000/customers/5",
+                        "phone_number": "555-1212",
+                        "address": "100 Endless Way",
+                        "user": "http://localhost:8000/users/6"
                     }
-                ]
-            """
+                },
+                {
+                    "id": 2,
+                    "seller": {
+                        "url": "http://localhost:8000/customers/6",
+                        "phone_number": "555-1212",
+                        "address": "100 Dauntless Way",
+                        "user": "http://localhost:8000/users/7"
+                    }
+                },
+                {
+                    "id": 3,
+                    "seller": {
+                        "url": "http://localhost:8000/customers/7",
+                        "phone_number": "555-1212",
+                        "address": "100 Indefatiguable Way",
+                        "user": "http://localhost:8000/users/8"
+                    }
+                }
+            ]
+        """
         customer = Customer.objects.get(user=request.auth.user)
-        favorites = Favorite.objects.filter(customer=customer)
+        if request.method == "GET":
 
-        serializer = FavoriteSerializer(
-            favorites, many=True, context={'request': request})
-        return Response(serializer.data)
+           
+            favorites = Favorite.objects.filter(customer=customer)
 
-    if request.method == "POST":
-        new_favorite = Favorite()
-        new_favorite.customer = Customer.objects.get(user=request.auth.user)
-        new_favorite.seller = Customer.objects.get(favorited_seller__id=request.data['seller'])
+            serializer = FavoriteSerializer(
+                favorites, many=True, context={'request': request})
+            return Response(serializer.data)
 
-        new_favorite.save()  
 
-        serializer = FavoriteSerializer(
-            new_favorite, many=False, context={'request': request})
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if request.method == "POST":
+            seller_id = self.request.data["seller"]
+            if seller_id is not None:
+                seller = Customer.objects.get(pk=seller_id)
+                try:
+                    favorite = Favorite.objects.get(customer=customer, seller=seller)
+                except Favorite.DoesNotExist as ex:
+                    new_favorite = Favorite()
+                    new_favorite.customer = customer
+                    new_favorite.seller = seller
+                    new_favorite.save()
+        
+                    serializer = FavoriteSerializer(
+                        new_favorite, context={'request': request})
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+                serializer = FavoriteSerializer(
+                        favorite, context={'request': request})
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class LineItemSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for products
