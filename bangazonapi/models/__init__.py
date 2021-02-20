6c3af5195f15f0676d171ed76007a60c8ffd9968
Joshua Medlen <joshua246@gmail.com>
2021-02-20 10:17:56 -0600

    in process. working on a authentication error

--- bangazonapi/models/__init__.py
+++ bangazonapi/models/__init__.py
@@ -8,3 +8,4 @@ from .recommendation import Recommendation
 from .rating import Rating
 from .favorite import Favorite
 from .productrating import ProductRating
+from .product_likes import ProductLikes
